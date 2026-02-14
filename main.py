96e399e5143f528e51118346389cc66498cef559
Likithkg <109513581+Likithkg@users.noreply.github.com>
2026-02-14 17:34:15 +0530

    minnor chages in main

--- main.py
+++ main.py
@@ -72,10 +72,10 @@ def root():
         "health": "/health"
     }
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8000,
+#         reload=True
+#     )
